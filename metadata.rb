7ecc37ffe5ab54cb88c7625637a41bc73794bcb8
Mathieu Sauve-Frankel <msf@kisoku.net>
2016-04-04 16:36:24 +0900

    bump for release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'msf@kisoku.net'
 license 'apache2'
 description 'manage github organizations with poise'
 long_description 'manage github organizations with poise'
-version '0.2.1'
+version '0.2.2'
 issues_url 'https://github.com/kisoku/poise-github/issues'
 source_url 'https://github.com/kisoku/poise-github'
 
